28b24ca0900d99debe2f936e2dbbff5fd88985cf
Nerea <nerea.ribes-karabay@yh.nackademin.se>
2016-05-10 12:37:16 +0200

    Add new class

---
+++ MyApp/MyApp/SummerHot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyApp
+{
+    public class SummerHot
+    {
+
+        //Här jobbar vi i develop branch
+    }
+}
